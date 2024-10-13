d2161cc62c2a530663bbdcc995aad3ddb71eb0e3
deepnav4 <workwithdeepnav@gmail.com>
2024-10-13 19:13:43 +0530

    Update 4_Pivot_Element_In_An_Array.cpp

--- Lecture-013/4_Pivot_Element_In_An_Array.cpp
+++ Lecture-013/4_Pivot_Element_In_An_Array.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int pivotElement(int arr[], int size) {
-    int sum[100] = {0};
-
-    for(int i=0; i<size; i++) {
-        sum[i+1] = sum[i] + arr[i];
+int pivotEl(int arr[],int size){
+    int totalsum = 0;
+    int leftSum = 0;
+    int rightSum = 0;
+    int sumArray[size];
+    for(int i=0; i<size; i++){
+        totalsum += arr[i];
     }
-
-    for(int i=0; i<size; i++) {
-        int left = sum[i];
-        int right = sum[size] - sum[i+1];
-
-        if(left == right) {
-            return arr[i];
-        }
+    for(int i=0; i<size; i++){
+        rightSum = totalsum - leftSum - arr[i];
+        if(leftSum == rightSum)
+            return i;
+        else
+            leftSum = leftSum + arr[i];
     }
-
     return -1;
 }
 
-int main() {
-    int n, element;
-    int arr[100];
-
-    cout<<"Enter the size of the array : ";
-    cin>>n;
-
-    cout<<"Enter the elements of tha array : ";
-    for(int i=0; i<n; i++) {
-        cin>>arr[i];
-    }
-
-    int pivot = pivotElement(arr, n);
-
-    cout<<"Pivot element of the array : "<<pivot;
-
-    return 0;
+int main(){
+    int arr[] = {2,1,-1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int result = pivotEl(arr,size);
+    result != -1 ? (cout<<"The Pivot Index of Given Array is : " <<pivotEl(arr,size)):cout<<"No Pivot Index Exist";
 }
